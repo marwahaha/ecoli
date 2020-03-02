549983c9811619a76a7d47ffa45e37dac5579256
Kunal Marwaha <marwahaha@berkeley.edu>
2020-03-02 14:29:16 -0800

    Extract code in while loop to a function

--- angie.R
+++ angie.R
@@ -7,30 +7,39 @@ ecoli_all <- ChemFieldLab2018 %>%
   filter(SampleType == "Regular")
 
 
+analyze_90_day_window <- function(ecoli_all, start, end, TEN_PERCENT_THRESHOLD, GEOMETRIC_MEAN_THRESHOLD) {
+  # Get data in 90-day window
+  ecoli_90 <- ecoli_all %>%
+    filter(Date > start) %>%
+    filter(Date <= end)
+
+  # Summarize and check thresholds
+  ecoli_summary <- ecoli_90 %>%
+    group_by(Year, WaterBodyReport, SiteCode, SiteClassification)%>%
+    summarise(
+      NumResults = n(),
+      NumResultsTenPercent = 0.1*n(),
+      NumResultsAboveTenPercentThreshold = sum(c(ResultValue > TEN_PERCENT_THRESHOLD)),
+      RuleTenPercent = NumResultsAboveTenPercentThreshold <= NumResultsTenPercent,
+      geometric_mean = exp(mean(log(ResultValue))),
+      RuleGeometricMean= geometric_mean <= GEOMETRIC_MEAN_THRESHOLD,
+      Passing= RuleTenPercent&RuleGeometricMean,
+    )
+
+  return(ecoli_summary)
+}
+
+
 get_ecoli_results <- function(ecoli_all, TEN_PERCENT_THRESHOLD, GEOMETRIC_MEAN_THRESHOLD) {
-  # Limiting to last 90 days, then moving the 90-day window back by 1 day each time.
+  # The initial 90-day window ends on the date of the last sample.
+  # The window moves back by 1 day each time until it includes the date of the first sample.
   ecoli_full_results <- c() 
   end <- max(ecoli_all$Date)
   start <- end - 90
   while (start >= (min(ecoli_all$Date) - 1)) {
-    # Get data in 90-day window
-    ecoli_90 <- ecoli_all %>%
-      filter(Date > start) %>%
-      filter(Date <= end)
-    
-    ecoli_summary <- ecoli_90 %>%
-      group_by(Year, WaterBodyReport, SiteCode, SiteClassification)%>%
-      summarise(
-        NumResults = n(),
-        NumResultsTenPercent = 0.1*n(),
-        NumResultsAboveTenPercentThreshold = sum(c(ResultValue > TEN_PERCENT_THRESHOLD)),
-        RuleTenPercent = NumResultsAboveTenPercentThreshold <= NumResultsTenPercent,
-        geometric_mean = exp(mean(log(ResultValue))),
-        RuleGeometricMean= geometric_mean <= GEOMETRIC_MEAN_THRESHOLD,
-        Passing= RuleTenPercent&RuleGeometricMean,
-      )
-    
     # Decide if rules pass for this 90-day window, and put it in full results.
+    ecoli_summary <- analyze_90_day_window(ecoli_all, start, end, TEN_PERCENT_THRESHOLD, GEOMETRIC_MEAN_THRESHOLD)
+
     ecoli_result <- ecoli_summary %>%
       select(Year, WaterBodyReport, SiteCode, SiteClassification, Passing)
     ecoli_full_results <- rbind(ecoli_full_results,  ecoli_result)
@@ -49,3 +58,5 @@ get_ecoli_results <- function(ecoli_all, TEN_PERCENT_THRESHOLD, GEOMETRIC_MEAN_T
 }
 
 out <- get_ecoli_results(ecoli_all, 236, 64)
+
+failing <- out %>% filter(success != TRUE)
